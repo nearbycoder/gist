85c3087ebd1427695d6a849223a54f2c71d13fb0
Josh Hamilton <nearbycoder@gmail.com>
2025-01-04 11:48:23 -0600

    convert to use middlware

--- app/serverFunctions/gists.ts
+++ app/serverFunctions/gists.ts
@@ -1,12 +1,11 @@
-import { createServerFn } from '@tanstack/start';
+import { createMiddleware, createServerFn } from '@tanstack/start';
 import { z } from 'zod';
 import { zodValidator } from '@tanstack/zod-adapter';
 import { prisma } from '@/libs/db';
 import { useAppSession } from '@/libs/session';
 
-export const getGists = createServerFn({
-  method: 'GET',
-}).handler(async () => {
+// Middleware
+const authMiddleware = createMiddleware().server(async ({ next }) => {
   const session = await useAppSession();
   const user = await prisma.user.findUnique({
     where: { email: session.data.userEmail },
@@ -16,48 +15,125 @@ export const getGists = createServerFn({
     throw new Error('User not found');
   }
 
-  return await prisma.gist.findMany({
-    where: {
-      userId: user.id,
-    },
-    include: {
-      versions: true,
+  return next({
+    context: {
+      user,
     },
   });
 });
 
-export const deleteGist = createServerFn({
-  method: 'POST',
+// GET /api/gists
+export const getGists = createServerFn({
+  method: 'GET',
 })
-  .validator(
-    z.object({
-      gistId: z.string(),
-    })
-  )
-  .handler(async ({ data }) => {
-    const session = await useAppSession();
-    const user = await prisma.user.findUnique({
-      where: { email: session.data.userEmail },
-    });
+  .middleware([authMiddleware])
+  .handler(async ({ context }) => {
+    const user = context.user;
 
-    if (!user) {
-      throw new Error('User not found');
-    }
-
-    await prisma.version.deleteMany({
+    return await prisma.gist.findMany({
       where: {
-        gistId: data.gistId,
+        userId: user.id,
+      },
+      include: {
+        versions: true,
+      },
+    });
+  });
+
+// GET /api/gists/:id
+export const getGist = createServerFn({
+  method: 'GET',
+})
+  .middleware([authMiddleware])
+  .validator(
+    zodValidator(
+      z.object({
+        id: z.string(),
+      })
+    )
+  )
+  .handler(async ({ data, context }) => {
+    const user = context.user;
+
+    const gist = await prisma.gist.findUnique({
+      where: { id: data.id, userId: user.id },
+      include: {
+        versions: {
+          orderBy: {
+            version: 'desc',
+          },
+        },
       },
     });
 
-    await prisma.gist.delete({
-      where: { id: data.gistId, userId: user.id },
-    });
-
-    return;
+    return gist;
   });
 
+// GET /api/gists/:id/public
+export const getPublicGist = createServerFn({
+  method: 'GET',
+})
+  .validator(
+    zodValidator(
+      z.object({
+        id: z.string(),
+      })
+    )
+  )
+  .handler(async ({ data }) => {
+    const gist = await prisma.gist.findUnique({
+      where: { id: data.id, isPublic: true },
+      include: {
+        versions: {
+          orderBy: {
+            version: 'desc',
+          },
+        },
+      },
+    });
+
+    return gist;
+  });
+
+// POST /api/gists
+export const createGist = createServerFn({ method: 'POST' })
+  .middleware([authMiddleware])
+  .validator(
+    zodValidator(
+      z.object({
+        title: z.string(),
+        body: z.string(),
+        language: z.string(),
+        isPublic: z.boolean(),
+      })
+    )
+  )
+  .handler(async ({ data, context }) => {
+    const user = context.user;
+
+    const gist = await prisma.gist.create({
+      data: {
+        title: data.title,
+        language: data.language,
+        isPublic: data.isPublic,
+        userId: user.id,
+      },
+    });
+
+    await prisma.version.create({
+      data: {
+        version: 1,
+        body: data.body,
+        gistId: gist.id,
+      },
+    });
+
+    return gist;
+  });
+
+// PATCH /api/gists/:id
 export const updateGist = createServerFn({ method: 'POST' })
+  .middleware([authMiddleware])
   .validator(
     zodValidator(
       z.object({
@@ -69,15 +145,8 @@ export const updateGist = createServerFn({ method: 'POST' })
       })
     )
   )
-  .handler(async ({ data }) => {
-    const session = await useAppSession();
-    const user = await prisma.user.findUnique({
-      where: { email: session.data.userEmail },
-    });
-
-    if (!user) {
-      throw new Error('User not found');
-    }
+  .handler(async ({ data, context }) => {
+    const user = context.user;
 
     const versions = await prisma.version.findMany({
       where: { gistId: data.id },
@@ -106,98 +175,28 @@ export const updateGist = createServerFn({ method: 'POST' })
     return gist;
   });
 
-export const createGist = createServerFn({ method: 'POST' })
+// DELETE /api/gists/:id
+export const deleteGist = createServerFn({
+  method: 'POST',
+})
+  .middleware([authMiddleware])
   .validator(
-    zodValidator(
-      z.object({
-        title: z.string(),
-        body: z.string(),
-        language: z.string(),
-        isPublic: z.boolean(),
-      })
-    )
+    z.object({
+      gistId: z.string(),
+    })
   )
-  .handler(async ({ data }) => {
-    const session = await useAppSession();
-    const user = await prisma.user.findUnique({
-      where: { email: session.data.userEmail },
-    });
+  .handler(async ({ data, context }) => {
+    const user = context.user;
 
-    if (!user) {
-      throw new Error('User not found');
-    }
-
-    const gist = await prisma.gist.create({
-      data: {
-        title: data.title,
-        language: data.language,
-        isPublic: data.isPublic,
-        userId: user.id,
+    await prisma.version.deleteMany({
+      where: {
+        gistId: data.gistId,
       },
     });
 
-    await prisma.version.create({
-      data: {
-        version: 1,
-        body: data.body,
-        gistId: gist.id,
-      },
+    await prisma.gist.delete({
+      where: { id: data.gistId, userId: user.id },
     });
 
-    return gist;
-  });
-
-export const getGist = createServerFn()
-  .validator(
-    zodValidator(
-      z.object({
-        id: z.string(),
-      })
-    )
-  )
-  .handler(async ({ data }) => {
-    const session = await useAppSession();
-    const user = await prisma.user.findUnique({
-      where: { email: session.data.userEmail },
-    });
-
-    if (!user) {
-      throw new Error('User not found');
-    }
-
-    const gist = await prisma.gist.findUnique({
-      where: { id: data.id, userId: user.id },
-      include: {
-        versions: {
-          orderBy: {
-            version: 'desc',
-          },
-        },
-      },
-    });
-
-    return gist;
-  });
-
-export const getPublicGist = createServerFn()
-  .validator(
-    zodValidator(
-      z.object({
-        id: z.string(),
-      })
-    )
-  )
-  .handler(async ({ data }) => {
-    const gist = await prisma.gist.findUnique({
-      where: { id: data.id, isPublic: true },
-      include: {
-        versions: {
-          orderBy: {
-            version: 'desc',
-          },
-        },
-      },
-    });
-
-    return gist;
+    return;
   });
