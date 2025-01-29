7c52464f6d0846f8d46001518634e47bf11f4449
Josh Hamilton <nearbycoder@gmail.com>
2025-01-28 21:23:20 -0600

    add gravatar

--- app/components/ui/nav-user.tsx
+++ app/components/ui/nav-user.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { ChevronsUpDown, LogOut } from 'lucide-react';
+import Gravatar from 'react-gravatar';
 
 import { useRouter } from '@tanstack/react-router';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -42,7 +43,7 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <Gravatar email={user.email} />
                 <AvatarFallback className="rounded-lg">
                   {user.name
                     .split(' ')
@@ -66,7 +67,7 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <Gravatar email={user.email} />
                   <AvatarFallback className="rounded-lg">
                     {user.name
                       .split(' ')
