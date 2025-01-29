c94ba40bc87e943a35e1a9036bd153d07a76f45f
Josh Hamilton <nearbycoder@gmail.com>
2025-01-28 19:23:52 -0600

    update to latest

--- app/router.tsx
+++ app/router.tsx
@@ -4,7 +4,6 @@ import { routeTree } from './routeTree.gen';
 export function createRouter() {
   const router = createTanStackRouter({
     routeTree,
-    defaultPreload: 'intent',
   });
 
   return router;
