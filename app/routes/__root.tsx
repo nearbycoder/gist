f016b67bab6a34d3ac112de9b81b55785d80aec9
Josh Hamilton <nearbycoder@gmail.com>
2025-02-09 12:13:53 -0600

    Update __root.tsx

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -27,7 +27,7 @@ export const Route = createRootRoute({
       },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1 user-scalable=no',
+        content: 'width=device-width, initial-scale=1, user-scalable=no',
       },
       {
         title: 'Gist',
