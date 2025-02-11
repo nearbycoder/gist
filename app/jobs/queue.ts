acc553c2926bda53059b6ddb9ab5fb85d2cc5d68
Josh Hamilton <nearbycoder@gmail.com>
2025-02-10 21:23:31 -0600

    remove queue

--- app/jobs/queue.ts
+++
@@ -1,7 +0,0 @@
-import { better, defineQueue } from 'plainjob';
-import Database from 'better-sqlite3';
-import 'dotenv/config';
-
-const connection = better(new Database(process.env.WORKER_DATABASE_URL));
-
-export const queue = defineQueue({ connection });
